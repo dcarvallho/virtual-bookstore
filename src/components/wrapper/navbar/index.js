26574546cadb6738e3609406c059e603a00ddd98
Davi Augusto <davi_augusto11@hotmail.com>
2024-04-05 00:12:20 -0300

    feat: added buttons and elements in navbar

--- src/components/wrapper/navbar/index.js
+++ src/components/wrapper/navbar/index.js
@@ -1,16 +1,24 @@
-import { CORE_NAME } from '@/constants';
-import React from 'react'
+import { CORE_NAME } from "@/constants";
+import React from "react";
+import style from "./style.module.css";
 
 const Navbar = () => {
   return (
     <>
-        <nav>
-          {/* TODO: Quando efetuar o login, aparecer o nome do user no lugar do "Login" */}
-            <div>{CORE_NAME}</div>
-            <div>Login</div>
-        </nav>
+      <nav className={style.navbarMain}>
+        {/* TODO: Quando efetuar o login, aparecer o nome do user no lugar do "Login" */}
+        <div>{CORE_NAME}</div>
+        <div className={style.rightElements}>
+          <>
+            <span>Olá, Davi!</span>
+          </>
+          <div className={style.loginLogout}>
+            <a href="">Login</a>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
