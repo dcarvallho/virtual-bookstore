2913bc4323d3440f8cf98cafcfa6cab4176f4e98
Davi Augusto <davi_augusto11@hotmail.com>
2024-04-19 00:04:06 -0300

    chore: ajustar tamanho da tela inicial

--- src/components/wrapper/index.js
+++ src/components/wrapper/index.js
@@ -3,13 +3,14 @@ import FooterComponent from "./footer";
 import Navbar from "./navbar";
 import store from "@/redux/store";
 import { Provider } from "react-redux";
+import style from "./style.module.css";
 
 const WrapperComponent = ({ children }) => {
   return (
     <>
       <Provider store={store}>
         <Navbar />
-        <main>{children}</main>
+        <main className={style.mainContent}>{children}</main>
         <FooterComponent />
       </Provider>
     </>
