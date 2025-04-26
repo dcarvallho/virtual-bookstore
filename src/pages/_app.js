5a97f39a799f6e065ca4e1b032986c59046be633
Davi Augusto <davi_augusto11@hotmail.com>
2025-04-26 00:51:25 -0300

    feat: include and config pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import WrapperComponent from '@/components/wrapper';
 import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
     return (
         <WrapperComponent>
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#2196f3" />
+            </Head>
             <Component {...pageProps} />
         </WrapperComponent>
     );
